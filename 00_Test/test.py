bce36ff170af65e163c5fdcddc8105f12a343f5b
daesung7723 <daesung7723@gmail.com>
2024-08-07 09:27:32 +0900

    Tk Docs Link

--- 00_Test/test.py
+++ 00_Test/test.py
@@ -1,26 +1,38 @@
-import socket
-import urllib.request
+from tkinter import *
+from tkinter import ttk
 
-def get_internal_ip():
+def calculate(*args):
     try:
-        host_name = socket.gethostname()
-        internal_ip = socket.gethostbyname(host_name)
-        return internal_ip
-    except Exception as e:
-        return str(e)
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
 
-def get_external_ip():
-    try:
-        url = "https://api64.ipify.org?format=json"
-        response = urllib.request.urlopen(url)
-        data = response.read().decode("utf-8")
-        external_ip = data.split(':')[1].split('"')[1]
-        return external_ip
-    except Exception as e:
-        return str(e)
+root = Tk()
+root.title("Feet to Meters")
 
-internal_ip = get_internal_ip()
-external_ip = get_external_ip()
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-print("Int. IP :", internal_ip)
-print("Ext. IP :", external_ip)
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
